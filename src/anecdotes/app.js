7ea5f1125df18ae44c7125c2de0ec58456c47c06
mohamedisakr <mohamedisakr@gmail.com>
2020-05-28 12:47:17 +0200

    "finishing exercise 1.13*: anecdotes step2"

--- src/anecdotes/app.js
+++ src/anecdotes/app.js
@@ -3,21 +3,42 @@ import React, { useState } from "react";
 
 const App = (props) => {
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(
+    Array.from(Array(props.anecdotes.length), () => 0)
+  );
+
+  //   const voting = Array.from(Array(props.anecdotes.length), () => 0);
+  //   let newVoting = [];
+
+  const handleVoting = (event) => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+
+    console.log(votes[selected]);
+    console.log(votes.join(" "));
+  };
+
   const handleClick = (event) => {
     setSelected(Math.floor(Math.random() * props.anecdotes.length));
-    // selected === props.anecdotes.length - 1
-    //   ? setSelected(0)
-    //   : setSelected(selected + 1);
   };
+
   return (
     <div>
-      <div>{props.anecdotes[selected]}</div>
+      <div>{props.anecdotes[selected]}</div>{" "}
       <div>
+        <p>has {votes[selected]} votes</p>
+      </div>
+      <div>
+        <button type="button" onClick={handleVoting}>
+          Vote
+        </button>
         <button type="button" onClick={handleClick}>
           Next Anecdote
         </button>
       </div>
-      <div>{selected}</div>
+      <div>The selected index {selected}</div>
+      <div>The votes Array {votes.join(" ")}</div>
     </div>
   );
 };
