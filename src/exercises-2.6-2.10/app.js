9066878ae2dc269227bbb683c1162ffb65ab71a4
mohamedisakr <mohamedisakr@gmail.com>
2020-05-30 09:05:09 +0200

    implemting exercise 2.9* The Phonebook Step4 lesson (b) forms, from part 2

--- src/exercises-2.6-2.10/app.js
+++ src/exercises-2.6-2.10/app.js
@@ -3,9 +3,18 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([
     { id: 1, name: "Tamil", number: 1111 },
+    { id: 2, name: "Arto Hellas", number: "040-123456" },
+    { id: 3, name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: 4, name: "Dan Abramov", number: "12-43-234345" },
+    { id: 5, name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [keyword, setKeyword] = useState("");
+  let filteredContacts = persons.filter(({ name }) =>
+    name.toLowerCase().includes(keyword.toLowerCase())
+  );
+  console.log(filteredContacts);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -39,9 +48,24 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search:
+        <input
+          type="search"
+          placeholder="Search for contact"
+          value={keyword}
+          onChange={handleKeywordChange}
+        />
+        {keyword}
+      </div>
+      <h2>Add New Contact</h2>
       <form onSubmit={handleSubmit}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
@@ -49,14 +73,13 @@ const App = () => {
         <div>
           Number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
-        {newName}
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
+        {filteredContacts.map((person) => (
           <p key={person.id}>
             {person.name} {person.number}
           </p>
