502a7a765d8b4206bb6639565cde0bdef5c7b7ab
mohamedisakr <mohamedisakr@gmail.com>
2020-06-01 13:01:05 +0200

    use data service to get all contacts, lesson d from part 2

---
+++ src/exercises-2.15-2.18/components/app.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import ContactService from "./services";
+import ContactList from "./contact-list";
+import ContactForm from "./contact-form";
+import SearchBox from "./search-box";
+// import axios from "axios";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    ContactService.getAll().then((initiatedContacts) =>
+      setPersons(initiatedContacts)
+    );
+    // axios
+    //   .get("http://localhost:3001/persons")
+    //   .then((response) => setPersons(response.data));
+  }, []);
+
+  let filteredContacts = persons.filter(({ name }) =>
+    name.toLowerCase().includes(keyword.toLowerCase())
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      id: persons.length + 1,
+      name: newName,
+      number: newNumber,
+    };
+
+    if (
+      persons.find(
+        ({ name }) => name.toLowerCase() === personObject.name.toLowerCase()
+      )
+    ) {
+      alert(`${personObject.name} is already added to phonebook`);
+      setNewName("");
+      setNewNumber("");
+      return;
+    }
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  return (
+    <div>
+      <SearchBox
+        keyword={keyword}
+        placeholder="Search for contact"
+        handleKeywordChange={handleKeywordChange}
+      />
+      <ContactForm
+        newName={newName}
+        newNumber={newNumber}
+        handleSubmit={handleSubmit}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <ContactList contacts={filteredContacts} />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./exercises-2.11-2.14/countries-rest-api/app";
+import App from "./exercises-2.15-2.18/components/app";
 
 render(<App />, document.querySelector("#root"));
